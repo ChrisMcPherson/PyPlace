b3804e45a7f86d1d71d18d4908f39abf45257cf6
ChrisMcPherson <summernightdrive@gmail.com>
2015-07-26 14:23:57 -0400

    Test Python environment with api call

--- PyWeather/PyWeather.py
+++ PyWeather/PyWeather.py
@@ -1,2 +1,9 @@
-print('Hello World')
-print('Test')
+﻿from urllib import Request, urlopen, URLError
+
+request = Request('http://www.placekittens.com/')
+try:
+    response = urlopen(request)
+    kittens = response.read()
+    print(kittens[559:1000])
+except URLError as e:
+    print('no kittens because error', e)
