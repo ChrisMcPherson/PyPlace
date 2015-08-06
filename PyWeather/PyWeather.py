e4664f6082b613e9d75445a64529ad724ca7ba0a
ChrisMcPherson <summernightdrive@gmail.com>
2015-08-05 21:01:02 -0400

    Simplified header and attribute list build.

--- PyWeather/PyWeather.py
+++ PyWeather/PyWeather.py
@@ -1,55 +1,56 @@
-﻿
-from urllib.request import urlopen
+﻿from urllib.request import urlopen
 from xml.etree import ElementTree as ET
 import os.path
 
-def getDays(xmlTree, fileExists):
-    forecastDays = []
-    if fileExists == 0:
-        forecastDays.append(['Date']) #data header
+def getDays(xmlTree, dataSet):
+    """
+    Returns a list with sublists for each day of the forecast 
+    """
+    dayInForecast = 1
     for el in xmlTree.findall('.//time'):
         day = []
         day.append(el.attrib.get('day'))
-        forecastDays.append(day)
-    return forecastDays
+        day.append(dayInForecast)
+        dataSet.append(day)
+        dayInForecast += 1
+    return dataSet
 
-def addAttribute(xmlTree, list, element, attribute, header, fileExists):
-    if fileExists == 0:
-        spam = 1
-        for i, head in enumerate(list): #add attribute header
-            if i == 0:
-                head.append(header)
+def addAttribute(xmlTree, dataSet, element, attribute, fileExists):
+    """
+    Appends weather attributes to each day of the forecast 
+    """
+    if not fileExists:
+        headerIndex = 1
     else:
-        spam = 0
+        headerIndex = 0
     for el in xmlTree.findall('.//'+element): #find attribute value for each day
-        for ix, day in enumerate(list): #find correct day to append attribute
-            if ix == spam:
+        for ix, day in enumerate(dataSet): #find correct day to append attribute
+            if ix == headerIndex:
                 day.append(el.attrib.get(attribute))
-        spam += 1
-    return list
+        headerIndex += 1
+    return dataSet
 
 path = 'C:\\Users\\Chris\\Desktop\\ForecastData.txt'
-fileExists = 0
+headers = ['day_in_forecast','Date','weather_description','precipitation','wind_direction','wind_speed_mps','temperature','pressure_hpa','humidity_pct','clouds_pct']
+attributes = [['symbol', 'name'],['precipitation', 'value'],['windDirection', 'code'],['windSpeed', 'mps'],
+              ['temperature', 'day'],['pressure', 'value'],['humidity', 'value'],['clouds', 'all']]
+fileExists = False
+dataSet = []
 
 if os.path.isfile(path):
     forecastFile = open(path,'r+')
-    fileExists = 1
+    fileExists = True
 else:
     forecastFile = open(path, 'w')
-
+    dataSet.append(headers)
 
 weatherAPI = 'http://api.openweathermap.org/data/2.5/forecast/daily?q=Cleveland,us&mode=xml&units=imperial&cnt=10'
 tree = ET.parse(urlopen(weatherAPI))
 
-dataSet = getDays(tree, fileExists) #assign list to sub-lists containing each day of the 10 day forecast 
+dataSet = getDays(tree, dataSet) #assign list to sub-lists containing each day of the 10 day forecast 
 
-dataSet = addAttribute(tree, dataSet, 'symbol', 'name', 'weather_description', fileExists) #Parse through forecast attributes. Add/Remove to change data set
-dataSet = addAttribute(tree, dataSet, 'precipitation', 'value', 'precipitation', fileExists) 
-dataSet = addAttribute(tree, dataSet, 'windDirection', 'code', 'wind_direction', fileExists) 
-dataSet = addAttribute(tree, dataSet, 'windSpeed', 'mps', 'wind_speed_mps', fileExists) 
-dataSet = addAttribute(tree, dataSet, 'temperature', 'day', 'temperature', fileExists) 
-dataSet = addAttribute(tree, dataSet, 'pressure', 'value', 'pressure_hpa', fileExists) 
-dataSet = addAttribute(tree, dataSet, 'humidity', 'value', 'humidity_pct', fileExists) 
-dataSet = addAttribute(tree, dataSet, 'clouds', 'all', 'clouds_pct', fileExists) 
+for i in attributes:
+    dataSet = addAttribute(tree, dataSet, i[0], i[1], fileExists)
+
 print(dataSet) #test
 
